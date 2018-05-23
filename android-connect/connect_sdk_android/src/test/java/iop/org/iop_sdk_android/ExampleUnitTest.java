2e2a1606ac80a39d798dd57af8ae6e0636e381c0
Franklin Marcano <franklinmarcano1970@hotmail.com>
2018-05-23 17:38:04 -0500

    Nuevos metodos

--- android-connect/connect_sdk_android/src/test/java/iop/org/iop_sdk_android/ExampleUnitTest.java
+++ android-connect/connect_sdk_android/src/test/java/iop/org/iop_sdk_android/ExampleUnitTest.java
@@ -1,6 +1,21 @@
 package iop.org.iop_sdk_android;
 
 import org.junit.Test;
+import org.libertaria.world.connection.DeviceNetworkConnection;
+import org.libertaria.world.core.IoPConnect;
+import org.libertaria.world.core.IoPConnectContext;
+import org.libertaria.world.crypto.CryptoWrapper;
+import org.libertaria.world.global.DeviceLocation;
+import org.libertaria.world.locnet.NodeInfo;
+import org.libertaria.world.profile_server.SslContextFactory;
+import org.libertaria.world.profile_server.engine.MessageQueueManager;
+import org.libertaria.world.profiles_manager.LocalProfilesDao;
+import org.libertaria.world.profiles_manager.PairingRequestsManager;
+import org.libertaria.world.profiles_manager.ProfilesManager;
+
+import java.util.List;
+
+import iop.org.iop_sdk_android.core.modules.profile.ProfilesModuleImp;
 
 import static org.junit.Assert.assertEquals;
 
@@ -13,5 +28,37 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void shouldBeGetListProfileServers() throws Exception {
+        IoPConnectContext contextWrapper = null;
+        CryptoWrapper cryptoWrapper = null;
+        SslContextFactory sslContextFactory = null;
+        LocalProfilesDao localProfilesDao = null;
+        ProfilesManager profilesManager = null;
+        PairingRequestsManager pairingRequestsManager = null;
+        DeviceLocation deviceLocation = null;
+        DeviceNetworkConnection deviceNetworkConnection = null;
+        MessageQueueManager messageQueueManager = null;
+
+        IoPConnect ioPConnect = new IoPConnect(contextWrapper,
+                cryptoWrapper,
+                sslContextFactory,
+                localProfilesDao,
+                profilesManager,
+                pairingRequestsManager,
+                deviceLocation,
+                deviceNetworkConnection,
+                messageQueueManager);
+        ProfilesModuleImp profilesModuleImp = new ProfilesModuleImp(null, ioPConnect, null, null);
+        List<NodeInfo> resultNodes =  profilesModuleImp.getProfileServers();
+
+        System.out.println("Found " + resultNodes.size() + " matching nodes");
+
+        for (NodeInfo node : resultNodes)
+            System.out.println("  " + node);
+
+        assert resultNodes.size() > 0;
     }
 }
